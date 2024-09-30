297ccff7748432cc83962e16d883cac7de91797d
anshravalll <anshraval@gmail.com>
2024-09-30 18:37:50 +0530

    Implemented logging

--- scraper.py
+++ scraper.py
@@ -2,18 +2,24 @@ import requests
 import json
 import os
 import uuid
+import logging
 from dotenv import load_dotenv
+from collections import Counter
 
 # Load environment variables from .env file
 load_dotenv()
 
+global counter
+counter = Counter()
 global search_query
 search_query = "Indian lehenga"
 
 def extract_urls(info):
     """Extract specific product URLs from the search results."""
     
+    logging.info("Initializing url and asin code extraction...") 
     products = info.get("results")
+    counter["total_product_urls"] = len(products)
     product_url_list = []
     
     with open("urls.txt", 'w') as file:
@@ -25,7 +31,10 @@ def extract_urls(info):
             if product_url:
                 file.write(product_url + '\n')
                 product_url_list.append((product_url, asin_code, product_uuid))
+                counter["extracted_product_urls"] += 1
     
+    logging.debug(f"From the list of {counter["total_product_urls"]} urls, {counter['extracted_product_urls']} are extracted.")
+    logging.info("url and asin code extraction completed.")
     print('URLs and ASINs provided.')
     return product_url_list
 
@@ -43,32 +52,59 @@ def create_product_info_directory(query = search_query):
 
 def extract_product_details(product_url):
     """Extract product details for every product in the product_url list."""
-    asin_code = product_url[0][1]
-    product_uuid = product_url[0][2]
-    response = requests_api(asin_code, query="")
-    response.raise_for_status()
-    info = response.json() 
-    info_folder_path = create_product_info_directory()
+    logging.info("Individual product extraction begins...")
+    products = []
+    for product in product_url:
+        asin_code = product[1] 
+        product_uuid = product[2]
+        response = requests_api(asin_code, query="")
+        response.raise_for_status()
+        info = response.json() 
+        products.append((info, product_uuid))
+        info_folder_path = create_product_info_directory()
+        full_path = f"{info_folder_path}/{product_uuid}.json" 
     
-    with open(f"{info_folder_path}/{product_uuid}.json", "w") as file:
-        json.dump(info, file, indent=4)
+        with open(full_path, "w") as file:
+            json.dump(info, file, indent=4)
     
+        counter["extracted_products"] += 1
+        logging.debug(f"{counter['extracted_products']} of {counter['extracted_product_urls']} product urls has been extracted at {full_path}")
+        print(f"{counter['extracted_products']} of {counter['extracted_product_urls']} extracted")
+    
+    if counter["extracted_products"] != counter["extracted_product_urls"]:
+        missing_product_num = counter["extracted_product_urls"] - counter["extracted_products"]
+        logging.warning(f"Out of {counter['extracted_product_urls']}, number of missed products are: {missing_product_num}")
+
+    else:
+        logging.info("Product detail extraction completed.")
+
     print("Product details extracted.")
-    return info, product_uuid
+    print("")
+    return products 
 
 def extract_images(product_url):
     """Extract images from the given product URLs."""
-    info, product_uuid = extract_product_details(product_url)
-    image_list = list(set(info.get("images")))
-    image_folder_path = create_product_directory(product_uuid)
+    products = extract_product_details(product_url)
+    for index, product in enumerate(products):
+        info, product_uuid = product[0], product[1]
+        image_list = list(set(info.get("images")))
+        counter["total_image_urls"] = len(image_list)
+        image_folder_path = create_product_directory(product_uuid)
     
-    for idx, image in enumerate(image_list):
-        response = requests.get(image)
-        if response.status_code == 200:
-            image_name = f"{product_uuid}_{idx + 1}.jpg"
-            with open(f"{image_folder_path}/{image_name}", "wb") as file:
-                file.write(response.content)
+        for idx, image in enumerate(image_list):
+            response = requests.get(image)
+            response.raise_for_status()
+            if response.status_code == 200:
+                image_name = f"{product_uuid}_{idx + 1}.jpg"
+
+                with open(f"{image_folder_path}/{image_name}", "wb") as file:
+                    file.write(response.content)
+
+                counter["extracted_images"] += 1
+                
+        logging.debug(f"{counter['extracted_images']}/{counter["total_image_urls"]} images are extracted for {index}/{counter['extracted_products']} products at path: {image_folder_path}")
     
+    logging.info("Images extracted successfully")
     print("Images downloaded successfully.")
 
 def requests_api(asin_code, query, product=True, domain='com', country='us', page='1'):
@@ -94,8 +130,12 @@ def requests_api(asin_code, query, product=True, domain='com', country='us', pag
 
     if product:
         response = requests.get(product_url, params=product_params)
+        logging.debug(f"Trying to get response object for product: {asin_code}")
     else:
         response = requests.get(search_url, params=search_params)
+        logging.debug(f"Trying to get response object for search query: {query}")
+
+    logging.debug(f"Got the respones with status code: {response.status_code}")
     
     return response
 
@@ -104,8 +144,18 @@ def generate_uuid():
 
 if __name__ == "__main__":
     """Main execution block to fetch search results and extract images."""
+    logging.basicConfig(
+        filename= "scraper.log",
+        filemode= "w",
+        encoding= "utf-8",
+        style= "{",
+        format= "[{asctime}] [{levelname}] [{funcName}: {lineno}] - {message}",
+        level= logging.DEBUG
+    )
+
+    logging.info("Initializing scraper...")
     response = requests_api(query = search_query, product=False, asin_code="")
-    
+
     if response.status_code == 200:
         info = response.json()
         with open("page.json", "a") as file:
@@ -117,5 +167,8 @@ if __name__ == "__main__":
             extract_images(product_url)
         except requests.exceptions.HTTPError as err:
             print(f"Error occurred: {err}")
+            logging.exception("View the stack trace below to catch the issue.")
     else:
         print(f"Request failed with status code: {response.status_code}")
+
+    logging.info("Stopping scraper execution.")
